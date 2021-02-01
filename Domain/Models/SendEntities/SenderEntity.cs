5dd12d603897d8516117080eab5e0fa511855b9a
MarckRDA <marcos.edo132@gmail.com>
2021-02-01 20:38:21 -0300

    Deleted useless code

--- Domain/Models/SendEntities/SenderEntity.cs
+++ Domain/Models/SendEntities/SenderEntity.cs
@@ -2,24 +2,13 @@ namespace Domain.Models.SendEntities
 {
     public class SenderEntity
     {
-        public string UserName { get; private set; }
-        public string UserEmail { get; private set; }
-        public string UserPhone { get; private set; }
-        public string ProductName { get; private set; }
-        public double ProductPrice { get; private set; }
-        public string ProductThumbImg { get; private set; }
-        public string ProductLinkRedirect { get; private set; }
-
-        public SenderEntity(string userName, string userEmail, string userPhone, string productName, double productPrice, string productThumbImg, string productLinkRedrect)
-        {
-            UserName = userName;
-            UserEmail = userEmail;
-            UserPhone = userPhone;
-            ProductName = productName;
-            ProductPrice = productPrice;
-            ProductThumbImg = productThumbImg;
-            ProductLinkRedirect = productLinkRedrect;
-        }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductThumbImg { get; set; }
+        public string ProductLinkRedirect { get; set; }
 
     }
 }
